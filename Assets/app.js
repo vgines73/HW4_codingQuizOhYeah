3bb494e58b72526e461cafb2b391c3c00852cea1
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-10-20 01:59:17 -0700

    added start button and function setup in javascript

--- Assets/app.js
+++ Assets/app.js
@@ -1,4 +1,24 @@
+
+// function for timer in header
 var secondsDisplay = document.querySelector("#seconds");
 
 var totalSeconds = 0;
 var secondsElasped = 0;
+
+// to start the game ill need
+
+//function to get to rules
+
+//function to get to questions
+
+//for loop for questions
+
+//function for correct points and total points stored and input on completed quiz and high scores
+
+//function for incorrect and reduce timer for each incorrect
+
+//function for completed quiz
+
+//function for high scores
+
+//function for play clear scores
