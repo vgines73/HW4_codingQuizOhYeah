daf7ce89684baf75c0e8224f4b7d71b0d5d8b4f1
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-10-20 23:00:49 -0700

    added variables questions setup

---
+++ Assets/js/app.js
@@ -0,0 +1,31 @@
+// to start the game ill need to
+
+//define all variables 
+
+var enterBtn = document.querySelector("#enter-btn");
+var rules = document.querySelector("#rules");
+var yeahBuddyBtn = document.querySelector("#yb-btn");
+var questions = document.querySelector("#questions");
+var questionsTitle = document.querySelector("#questions-title");
+var choices = document.querySelector("#choices");
+var completedQuiz = document.querySelector("#completed-quiz");
+var sumbitBtn = document.querySelector("#sbmt-btn");
+var highScores = document.querySelector("#high-scores");
+var playBtn = document.querySelector("#play-btn");
+var clearBtn = document.querySelector("#clear-btn");
+
+//function to get to rules
+
+//function to get to questions and start timer
+
+//for loop for questions
+
+//function for correct points and total points stored and input on completed quiz and high scores
+
+//function for incorrect and reduce timer for each incorrect
+
+//function for completed quiz
+
+//function for high scores
+
+//function for play and clear scores
---
+++ Assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "what is a string?",
+        choices: ["1", "number", "plain text", "a rope"],
+        answer: "plain text"
+    },
+    {
+        title: "what is a string?",
+        choices: ["1", "number", "plain text", "a rope"],
+        answer: "plain text"
+    },
+    {
+        title: "what is a string?",
+        choices: ["1", "number", "plain text", "a rope"],
+        answer: "plain text"
+    },
+    {
+        title: "what is a string?",
+        choices: ["1", "number", "plain text", "a rope"],
+        answer: "plain text"
+    },
+    {
+        title: "what is a string?",
+        choices: ["1", "number", "plain text", "a rope"],
+        answer: "plain text"
+    }
+];
