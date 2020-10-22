c9bef153a4ec84cdc623846ad71769958638f2ab
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-10-21 23:02:08 -0700

    can't generate next question

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -8,11 +8,16 @@ var yeahBuddyBtn = document.querySelector("#yb-btn");
 var questions = document.querySelector("#questions");
 var title = document.querySelector("#title");
 var choices = document.querySelector(".choices");
+var answerA = document.querySelector("#A");
+var answerB = document.querySelector("#B");
+var answerC = document.querySelector("#C");
+var answerD = document.querySelector("#D");
 var answer = document.querySelector(".answer");
 var completedQuiz = document.querySelector("#completed-quiz");
 var score = document.querySelector("#score")
 var sumbitBtn = document.querySelector("#sbmt-btn");
 var highScores = document.querySelector("#high-scores");
+var highScoresList = document.querySelector("#high-scores-list");
 var playBtn = document.querySelector("#play-btn");
 var clearBtn = document.querySelector("#clear-btn");
 
@@ -31,7 +36,7 @@ yeahBuddyBtn.addEventListener("click", startQuestions)
     
 
     // function to start timer
-    var sec = 5;
+    var sec = 60;
     var time = setInterval(startTimer, 1000);
 
     function startTimer() {
@@ -41,44 +46,65 @@ yeahBuddyBtn.addEventListener("click", startQuestions)
             clearInterval(time);
             questions.style.display = "none";
             completedQuiz.style.display = "block";
-        }
-        
-    };
-    
-}
+        }        
+    };    
 
+// function for user choice and points
+var score = 0;
+answerA.addEventListener("click", function () {
+    if (answerA === answer);
+    console.log(score);
+    score++;
+});
+
+answerB.addEventListener("click", function () {
+    if (answerB === answer);
+    console.log(score);
+    score++;
+});
+
+answerC.addEventListener("click", function () {
+    if (answerC === answer);
+    console.log(score);
+    score++;
+});
+
+answerD.addEventListener("click", function () {
+    if (answerD === answer);
+    console.log(score);
+    score++;
+});
 
 
 // for loop for questions
-    
-   for (let i = 0; i < q.length; i++) {
-       console.log(q[i]);  
-   };
-//for loop for choices
-    //for (let i = 0; i < choices.length; i++) {
-//console.log(choices[i]);
-        
-  // }
 
+//    for (var i = 0; i < q.length; i++) {
+//       console.log(q[i]);  
+//    };
+q.forEach(function(element){
+    console.log(element)
+});
 
-//function for correct points and total points stored and input on completed quiz and high scores
-var score = 0;
+}
+//function for total points stored and input on completed quiz and high scores
 
 
 
-//function for incorrect and reduce timer for each incorrect
+
+
 
 //function for completed quiz
 // sumbitBtn.addEventListener(click, highScores) //not working
 
+
+
+
+// //function for highScores
 //     function highScores () {
-//         completedQuiz.style.display = "none";
-//         highScores.style.display = "block";
-//     };
-
-
-// //function for high scores
+        // completedQuiz.style.display = "none";
+        // highScores.style.display = "block";
+    //};
 
 // //function for play and clear scores
-// playBtn.addEventListener("click", startQuestions)
+// playBtn.addEventListener("click", startQuestions) works
 // clearBtn.addEventListener("click", reset)
