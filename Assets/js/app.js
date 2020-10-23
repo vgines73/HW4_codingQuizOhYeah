b09ddce8b23ad70700122954631d38f65c4c100f
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-10-23 02:20:25 -0700

    fixed alert if finished early. Goes to next completed section

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -5,7 +5,6 @@ var enterBtn = document.querySelector("#enter-btn");
 var rulesPage = document.querySelector("#rules-page");
 var yeahBuddyBtn = document.querySelector("#yb-btn");
 var questionsPage = document.querySelector("#questions-page");
-var answer = document.querySelector(".answer");
 var completedQuiz = document.querySelector("#completed-quiz");
 var userScore = document.querySelector("#score")
 var sumbitBtn = document.querySelector("#sbmt-btn");
@@ -32,32 +31,35 @@ var optionA = document.querySelector("#optionA");
 var optionB = document.querySelector("#optionB");
 var optionC = document.querySelector("#optionC");
 var optionD = document.querySelector("#optionD");
+var sec = 60;
 
 //function to start the questions
 function startQuestions() {
     rulesPage.style.display = "none";
     questionsPage.style.display = "block";
     
-    title.textContent = myQuestions[questionsPointer].q;
-
     // alert for finishing the quiz early
+    
     if(questionsPointer === myQuestions.length){
         clearInterval(startTimer)
         alert("YEAH BUDDY! You survived and finished early " + sec + " seconds left");
-        return
+        questionsPage.style.display = "none";
+        completedQuiz.style.display = "block";
+        
     }
 
+    title.textContent = myQuestions[questionsPointer].q;
     optionA.textContent = myQuestions[questionsPointer].choices[0];
     optionB.textContent = myQuestions[questionsPointer].choices[1];
     optionC.textContent = myQuestions[questionsPointer].choices[2];
     optionD.textContent = myQuestions[questionsPointer].choices[3];
-
-      
+    
     // what happens if user picks correctly/incorrectly
     optionA.addEventListener("click", function () {
         if(optionA.getAttribute("data-answer") === myQuestions[questionsPointer].answer) {
             //console.log("this is correct")
             questionsPointer++;
+            userScore++;
             startQuestions()
         } else {
             //console.log("wrong")
@@ -70,6 +72,7 @@ function startQuestions() {
         if(optionB.getAttribute("data-answer") === myQuestions[questionsPointer].answer) {
             //console.log("this is correct")
             questionsPointer++;
+            userScore++;
             startQuestions()
         } else {
             //console.log("wrong")
@@ -82,6 +85,7 @@ function startQuestions() {
         if(optionC.getAttribute("data-answer") === myQuestions[questionsPointer].answer) {
             //console.log("this is correct")
             questionsPointer++;
+            userScore++;
             startQuestions()
         } else {
             //console.log("wrong")
@@ -94,6 +98,7 @@ function startQuestions() {
         if(optionD.getAttribute("data-answer") === myQuestions[questionsPointer].answer) {
             //console.log("this is correct")
             questionsPointer++;
+            userScore++;
             startQuestions()
         } else {
             //console.log("wrong")
@@ -103,18 +108,18 @@ function startQuestions() {
     })
 
     // function to start timer
-    var sec = 60;
+    sec = 60;
     var time = setInterval(startTimer, 1000);
 
     function startTimer() {
         document.getElementById("seconds").innerHTML = (sec + "seconds left");
         sec--;
-        if (sec < 0) {
-            time = 0;
+        if (sec === -1) {
             clearInterval(time);
             questionsPage.style.display = "none";
             completedQuiz.style.display = "block";
         }
+        
     };
 }
 //function for total points stored and input on completed quiz and high scores
@@ -126,7 +131,7 @@ function startQuestions() {
 
 //function for completed quiz page
 function getFinalScore () {
-    userScore.textContent = questionsPointer
+    userScore.textContent = userScore
     sumbitBtn.addEventListener(click, highScores) //not working
 };
 
