c13d8250a1437ad2d93b153880d013103c72af98
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-10-23 00:31:38 -0700

    finally got the questions to pop up

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,19 +1,19 @@
 // to start the game ill need to
 
 //define all variables 
-var timerStarts = document.querySelector(".time")
+//var timerStarts = document.querySelector(".time")
 var enterBtn = document.querySelector("#enter-btn");
-var rules = document.querySelector("#rules");
+var rulesPage = document.querySelector("#rules-page");
 var yeahBuddyBtn = document.querySelector("#yb-btn");
-var questions = document.querySelector("#questions");
-var title = document.querySelector("#title");
-var answerA = document.querySelector("#A");
-var answerB = document.querySelector("#B");
-var answerC = document.querySelector("#C");
-var answerD = document.querySelector("#D");
+var questionsPage = document.querySelector("#questions-page");
+//var title = document.querySelector("#title");
+//var answerA = document.querySelector("#A");
+//var answerB = document.querySelector("#B");
+//var answerC = document.querySelector("#C");
+//var answerD = document.querySelector("#D");
 var answer = document.querySelector(".answer");
 var completedQuiz = document.querySelector("#completed-quiz");
-var score = document.querySelector("#score")
+var userScore = document.querySelector("#score")
 var sumbitBtn = document.querySelector("#sbmt-btn");
 var highScores = document.querySelector("#high-scores");
 var highScoresList = document.querySelector("#high-scores-list");
@@ -22,18 +22,86 @@ var clearBtn = document.querySelector("#clear-btn");
 
 // function to get to rules
 enterBtn.addEventListener("click", showRules)
-   function showRules () {
-        enterBtn.style.display = "none";
-        rules.style.display = "block";
+function showRules() {
+    enterBtn.style.display = "none";
+    rulesPage.style.display = "block";
 };
 
-// function to start questions
+// function to start questions 
 yeahBuddyBtn.addEventListener("click", startQuestions)
-    function startQuestions () {
-        rules.style.display = "none";
-        questions.style.display = "block";
-        
+
+var questionsPointer = 0;
+
+var title = document.querySelector("#title");
+var optionA = document.querySelector("#optionA");
+var optionB = document.querySelector("#optionB");
+var optionC = document.querySelector("#optionC");
+var optionD = document.querySelector("#optionD");
+
+function startQuestions() {
+    rulesPage.style.display = "none";
+    questionsPage.style.display = "block";
     
+    title.textContent = myQuestions[questionsPointer].q;
+
+    if(questionsPointer === myQuestions.length){
+        clearInterval(startTimer)
+        alert("YEAH BUDDY! You finished early " + sec + " seconds left");
+        return
+    }
+    optionA.textContent = myQuestions[questionsPointer].choices[0];
+    optionB.textContent = myQuestions[questionsPointer].choices[1];
+    optionC.textContent = myQuestions[questionsPointer].choices[2];
+    optionD.textContent = myQuestions[questionsPointer].choices[3];
+
+      
+
+    optionA.addEventListener("click", function () {
+        if(optionA.getAttribute("data-answer") === myQuestions[questionsPointer].answer) {
+            console.log("this is correct")
+            questionsPointer++;
+            startQuestions()
+        } else {
+            console.log("wrong")
+            sec -= 10;
+        }
+    })
+
+    optionB.addEventListener("click", function () {
+        if(optionB.getAttribute("data-answer") === myQuestions[questionsPointer].answer) {
+            console.log("this is correct")
+            questionsPointer++;
+            startQuestions()
+        } else {
+            console.log("wrong")
+            sec -= 10;
+        }
+    })
+
+    optionC.addEventListener("click", function () {
+        if(optionC.getAttribute("data-answer") === myQuestions[questionsPointer].answer) {
+            console.log("this is correct")
+            questionsPointer++;
+            startQuestions()
+        } else {
+            console.log("wrong")
+            sec -= 10;
+        }
+    })
+
+    optionD.addEventListener("click", function () {
+        if(optionD.getAttribute("data-answer") === myQuestions[questionsPointer].answer) {
+            console.log("this is correct")
+            questionsPointer++;
+            startQuestions()
+        } else {
+            console.log("wrong")
+            sec -= 10;
+        }
+    })
+
+    
+
 
     // function to start timer
     var sec = 60;
@@ -42,75 +110,80 @@ yeahBuddyBtn.addEventListener("click", startQuestions)
     function startTimer() {
         document.getElementById("seconds").innerHTML = (sec + "seconds left");
         sec--;
-        if (sec == -1) {
+        if (sec === -1) {
             clearInterval(time);
-            questions.style.display = "none";
+            questionsPage.style.display = "none";
             completedQuiz.style.display = "block";
-        }        
-    };    
-
-// function for user choice and points
-var score = 0;
-answerA.addEventListener("click", function () {
-    if (answerA === answer) {
-    console.log(answerA);
-    score++;
-    } else {
-        score--;
-        sec--;
-    }
-});
-
-answerB.addEventListener("click", function () {
-    if (answerB === answer) {
-    console.log(score);
-    score++;
-    } else {
-        score--;
-        sec--;
-    }
-});
-
-answerC.addEventListener("click", function () {
-    if (answerC === answer){
-    console.log(score);
-    score++;
-    } else {
-        score--;
-        sec--;
-    }
-    
-});
-
-answerD.addEventListener("click", function () {
-    if (answerD === answer){
-    console.log(score);
-    score++;
-    } else {
-        score--;
-        sec--;
-    }
-});
-
-// target questions section div
-q = [];
-var questionDiv = document.getElementById("question");
-
-q.forEach(title => {
-    questionDiv.append(title);
-});
-// target choices section div
-var choicesDiv = document.getElementsByClassName(".choices");
-q.forEach(choices => {
-    choicesDiv.append(choices);
-});
-
-// target answer section div
-var answerDiv = document.getElementById("answer");
-q.forEach(answer => {
-    answerDiv.append(answer);
-})
+        }
+    };
 }
+
+//     // target questions section div
+//     // q = [];
+//     var questionDiv = document.getElementById("question");
+//     questionDiv.textContent()
+//     q.forEach(title => {
+//         questionDiv.append(title);
+        
+    
+//     });
+//     // target choices section div
+//     var choicesDiv = document.getElementsByClassName(".choices");
+//     q.forEach(choices => {
+//         choicesDiv.append(choices);
+//     });
+
+//     // target answer section div
+//     var answerDiv = document.getElementById("answer");
+//     q.forEach(answer => {
+//         answerDiv.append(answer);
+//     })
+// }
+// function for user choice and points
+// var score = 0;
+// var sec = 60;
+// answerA.addEventListener("click", function () {
+//     if (answerA === answer) {
+//         console.log(answerA);
+//         score++;
+//     } else {
+//         score--;
+//         sec--;
+//     }
+// });
+
+// answerB.addEventListener("click", function () {
+//     if (answerB === answer) {
+//         console.log(score);
+//         score++;
+//     } else {
+//         score--;
+//         sec--;
+//     }
+// });
+
+// answerC.addEventListener("click", function () {
+//     if (answerC === answer) {
+//         console.log(score);
+//         score++;
+//     } else {
+//         score--;
+//         sec--;
+//     }
+
+// });
+
+// answerD.addEventListener("click", function () {
+//     if (answerD === answer) {
+//         console.log(score);
+//         score++;
+//     } else {
+//         score--;
+//         sec--;
+//     }
+// });
+
+
 //    for (var i = 0; i < q.length; i++) {
 //       console.log(q[i]);  
 
